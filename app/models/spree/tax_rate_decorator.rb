d42bf267f7a37499025a1462522c37019fad3359
manishrawat0212 <manishrawat0212@gmail.com>
2017-02-02 22:11:40 +0530

    changes in tax_rate_decorator

--- app/models/spree/tax_rate_decorator.rb
+++ app/models/spree/tax_rate_decorator.rb
@@ -1,14 +1,14 @@
 module Spree
   TaxRate.class_eval do
-    def self.adjust_alcoholic_product(order, items)
+    def self.adjust_alcoholic_product(order, line_items)
       alcoholic_tax_category = TaxCategory.find_by(name: 'Alcohol')
       return if alcoholic_tax_category.nil?
 
       tax_rate_for_alcohol = alcoholic_tax_category.tax_rates.find_by(zone: order.tax_zone)
       return if tax_rate_for_alcohol.nil?
 
-      items.each do |item|
-        tax_rate_for_alcohol.adjust_alcoholic_product(order, item) if item.is_alcoholic?
+      line_items.each do |line_item|
+        tax_rate_for_alcohol.adjust_alcoholic_product(order, line_item) if line_item.is_alcoholic?
       end
     end
 
