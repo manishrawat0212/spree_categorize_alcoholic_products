f360e58bab11dd7d306f21609652aef83ec89cd1
manishrawat0212 <manishrawat0212@gmail.com>
2017-02-06 12:31:48 +0530

    alcoholic taxon note in variant edit form; changes in controller decorators

--- app/controllers/spree/admin/variants_controller_decorator.rb
+++ app/controllers/spree/admin/variants_controller_decorator.rb
@@ -2,15 +2,10 @@ module Spree
   module Admin
     VariantsController.class_eval do
       before_action :update_loaded_data, only: [:new, :create, :edit, :update]
-      before_action :load_alcohol_tax_category, only: [:new, :create, :edit, :update]
 
       private
         def update_loaded_data
           @tax_categories = TaxCategory.non_alcoholic
-        end
-
-        def load_alcohol_tax_category
-          @alcohol_tax_category = TaxCategory.find_by(name: ALCOHOLIC_TAX_CATEGORY)
         end
     end
   end
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,8 +1,12 @@
 module Spree
   Variant.class_eval do
-    before_save :update_tax_category_to_alcohol, if: Proc.new { |v| !v.is_master? && v.alcoholic? }
+    before_save :update_tax_category_to_alcohol, if: :product_is_alcohoic?
 
     delegate_belongs_to :product, :alcoholic?
+
+    def product_is_alcohoic?
+      !is_master? && alcoholic?
+    end
 
     def update_tax_category_to_alcohol
       self.tax_category = self.product.tax_category
