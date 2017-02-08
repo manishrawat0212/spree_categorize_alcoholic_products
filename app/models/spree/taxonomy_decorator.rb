21d9918dd43942cfdffbed476cedf32566a1f68d
manishrawat0212 <manishrawat0212@gmail.com>
2017-02-08 15:37:02 +0530

    taxonomy decorator callback issue fixed

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -2,7 +2,8 @@ Spree::Taxonomy.class_eval do
   before_destroy :update_associated_alcoholic_products, prepend: true
 
   def update_associated_alcoholic_products
-    products = Spree::Product.joins(:taxons).where(spree_products_taxons: {taxon_id: taxons.alcoholic.pluck(:id)})
-    products.update_as_non_alcoholic
+    associated_products = Spree::Product.where(id: taxons.map(&:product_ids).flatten)
+    still_alcoholic_product_ids = associated_products.joins(:taxons).where(spree_taxons: {alcoholic: true}).where.not(spree_taxons: {id: taxon_ids}).distinct.pluck(:id)
+    associated_products.where.not(id: still_alcoholic_product_ids).update_as_non_alcoholic
   end
 end
